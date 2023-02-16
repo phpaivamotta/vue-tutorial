d11eb9f95e4d3d3fd5e4a47e454bcba694f124ec
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2023-02-16 19:23:02 -0300

    finished lesson 6

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,28 @@
 export default {
     template: `
-        <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2" @click="toggleProcessing" :disabled="processing">
+        <button 
+            :class="{
+                'border rounded px-5 py-2': true,
+                'bg-blue-600 hover:bg-gray-400': type === 'primary',
+                'bg-purple-600 hover:bg-gray-400': type === 'secondary',
+                'bg-gray-200 hover:bg-gray-400': type === 'muted',
+                'is-loading': processing
+            }"
+            :disabled="processing"
+        >
             <slot />
         </button>
     `,
 
-    data () {
-        return {
-            processing: false
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+
+        processing: {
+            type: Boolean,
+            default: false
         }
     }
 }
