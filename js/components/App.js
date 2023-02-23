8681a2edfe21c0f778bfd70d33a76f71223d8a5d
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2023-02-22 23:33:37 -0300

    finished lesson 15

--- js/components/App.js
+++ js/components/App.js
@@ -1,9 +1,48 @@
 import Assignments from './Assignments.js'
+import Panel from './Panel.js'
 
 export default {
-    components: {Assignments},
+    components: {Assignments, Panel},
     
     template:`
-        <assignments></assignments>
+        <div class="grid gap-6">
+            <assignments></assignments>
+        
+            <panel>
+                We're in business.
+            </panel>
+
+            <panel>
+                <template v-slot:heading>
+                    Hello Bitch,
+                </template>
+
+                We're in business.
+            </panel>
+
+            <panel>
+                <template v-slot:heading>
+                    Hello Bitch,
+                </template>
+
+                We're in business.
+
+                <template v-slot:footer>
+                    This is my footer
+                </template>
+            </panel>
+
+            <panel theme="light">
+                <template v-slot:heading>
+                    Hello Bitch,
+                </template>
+
+                We're in business.
+
+                <template v-slot:footer>
+                    This is my footer
+                </template>
+            </panel>
+        </div>
     `,
 }
